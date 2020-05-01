af67d8752e371e045dbe23aef00a0a513cbfb328
lynix90 <lynix90@gmail.com>
2020-05-01 23:07:41 +0400

    не работает на meizu, идентично курсу

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,7 +3,11 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -15,8 +19,8 @@ public class FirstTest {
     {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName","Android");
-        capabilities.setCapability("deviceName","Nexus5x");
-        capabilities.setCapability("platformVersion","6.0.1");
+        capabilities.setCapability("deviceName","PRO7");
+        capabilities.setCapability("platformVersion","7.0");
         capabilities.setCapability("automationName","Appium");
         capabilities.setCapability("appPackage","org.wikipedia");
         capabilities.setCapability("appActivity","main.MainActivity");
@@ -32,6 +36,18 @@ public class FirstTest {
     @Test
     public void myTest()
     {
-        System.out.println("Test OK");
+        WebElement element_to_init_search = driver.findElementByXPath("//*[contains(@text, 'Search Wikipedia')]");
+        element_to_init_search.click();
+        WebElement element_to_search_line = waitForElementPresentByXpath("//*[contains (@text,'Search…')]" , "Cannot find input" , 10);
+        element_to_search_line.sendKeys("Java");
+    }
+    private WebElement waitForElementPresentByXpath(String xpath,String error_message, long timeoutInSeconds)
+    {
+        WebDriverWait wait=new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        By by = By.xpath(xpath);
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
     }
 }
